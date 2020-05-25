84e6c81fa1ef67e60928621f4f51a75dcfc71bea
espkishore <espkishore@gmail.com>
2020-05-25 15:15:14 +0530

    Update pdf_to_text.py

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -2,11 +2,11 @@ import PyPDF2
 
 pdfFileObj = open('pypdf1.pdf', 'rb')
 pdfReader =PyPDF2.PdfFileReader(pdfFileObj)
-x=pdfReader.numPages
-print(x)
+x=pdfReader.numPages//counting number of pages in file
+print(x)//displays total page number in file
 for i in range(0, x+1 ):
-    pageObj = pdfReader.getPage(i)
-    text=pageObj.extractText()
-    file1=open(r"I:\All\P\python practise\1.txt","a")
+    pageObj = pdfReader.getPage(i)//gets page number from file
+    text=pageObj.extractText()//it will extract text and places to variable
+    file1=open(r"I:\All\P\python practise\1.txt","a")//place the directory link where you want to download the text file, a is the command used to append extracted text in text file
     file1.writelines(text)
     file1.close()
